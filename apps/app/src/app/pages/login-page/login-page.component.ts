c50b55b07dec3c384ca6cce0fa4e5356c2b212d2
b.bonnin <b.bonnin@efysoft.fr>
2023-01-05 12:14:35 +0100

    login

--- apps/app/src/app/pages/login-page/login-page.component.ts
+++ apps/app/src/app/pages/login-page/login-page.component.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  FormBuilder,
+  FormControl,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent {
+  loginForm = this.formBuilder.group({
+    login: new FormControl<string>('', [Validators.required, Validators.email]),
+    password: new FormControl<string>('', Validators.required),
+  });
 
+  get login() {
+    return this.loginForm.get('login');
+  }
+
+  constructor(private readonly formBuilder: FormBuilder) {}
+
+  doLogin() {
+    console.log(this.loginForm.value);
+  }
 }
