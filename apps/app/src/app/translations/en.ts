c71341524ce440a73d44edb971ac9bfb8ba31f4d
Jules Dupuis <jules.dupuis@ynov.com>
2023-02-02 11:05:27 +0100

    add translations

--- apps/app/src/app/translations/en.ts
+++ apps/app/src/app/translations/en.ts
@@ -6,4 +6,20 @@ export const en = {
       loginLabel: 'Login',
     },
   },
+  register: {
+    title: 'Register',
+    hello: 'Hello',
+  },
+  home: {
+    title: 'Welcome',
+  },
+  hunt:{
+    title:'Hunt pokemon',
+  },
+  profil: {
+    title:'My profile',
+    form: {
+      emailLabel: 'My Email',
+    },
+  },
 };
