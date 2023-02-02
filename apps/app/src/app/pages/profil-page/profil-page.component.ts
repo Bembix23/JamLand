e95722cd0c15d8d532c6ae67cfb27b680122fff6
b.bonnin <b.bonnin@efysoft.fr>
2023-02-02 15:31:28 +0100

    Photos

--- apps/app/src/app/pages/profil-page/profil-page.component.ts
+++ apps/app/src/app/pages/profil-page/profil-page.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
 import { Auth } from '@angular/fire/auth';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { filter, map, switchMap, take } from 'rxjs';
+import { filter, map, Subject, switchMap, take } from 'rxjs';
 import { Observable } from 'rxjs';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import {
   FormBuilder,
   FormControl,
@@ -15,12 +15,20 @@ import {
 } from '@angular/forms';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { AuthService } from '../../services/auth.service';
+import { WebcamImage, WebcamModule } from 'ngx-webcam';
+import {
+  getDownloadURL,
+  ref,
+  Storage,
+  uploadBytes,
+} from '@angular/fire/storage';
 
 
 @Component({
   selector: 'app-profil-page',
   standalone: true,
   imports: [
+    WebcamModule,
     CommonModule,
     MatCardModule,
     MatInputModule,
@@ -32,6 +40,10 @@ import { AuthService } from '../../services/auth.service';
   templateUrl: './profil-page.component.html',
   styleUrls: ['./profil-page.component.scss'],
 })
+
+
+
+
 export class ProfilPageComponent {
   // infoUser$: Observable<Info>;
   isLoggedIn$: Observable<boolean> = this.authService.isLoggedIn$;
@@ -44,6 +56,9 @@ export class ProfilPageComponent {
     trading: new FormControl<boolean>(false),
     viewOther: new FormControl<boolean>(false),
   });
+  trigger$ = new Subject<void>();
+  @ViewChild('filePreview')
+private filePreview!: ElementRef;
 
   get profil() {
     return this.profilForm.get('profil');
@@ -53,7 +68,8 @@ export class ProfilPageComponent {
     private readonly formBuilder: FormBuilder,
     private readonly translateService: TranslateService,
     private readonly authService: AuthService,
-    private readonly auth: Auth
+    private readonly auth: Auth,
+    private readonly storage: Storage,
   ) {
     this.authService.user$
       .pipe(
@@ -96,5 +112,46 @@ export class ProfilPageComponent {
   }
   logout() {
     this.auth.signOut();
+  }
+  takePhoto() {
+    this.trigger$.next();
+  }
+
+  handleSnapshot($event: WebcamImage) {
+    this.filePreview.nativeElement.src = $event.imageAsDataUrl;
+
+    const base64Img = $event.imageAsBase64;
+
+    const u8arr = this.toByteArray(base64Img);
+    const filename = 'tmp.jpeg';
+    const file: File = new File([u8arr], filename, { type: 'image/jpeg' });
+
+    const fileDetails = ref(this.storage, filename);
+    uploadBytes(fileDetails, file)
+      .then(() => {
+        return getDownloadURL(fileDetails);
+      })
+      .then((url) => console.log(url));
+
+    /** As observable  :
+     * from(uploadBytes(fileDetails, file))
+     *       .pipe(switchMap(() => getDownloadURL(fileDetails)))
+     *       .subscribe((url) => console.log(url));
+     */
+  }
+
+  hasPreview() {
+    const preview = this.filePreview?.nativeElement?.src;
+    return preview && !preview.includes('#');
+  }
+
+  private toByteArray(base64Img: string) {
+    const bstr = atob(base64Img);
+    let n = bstr.length;
+    const u8arr = new Uint8Array(n);
+    while (n--) {
+      u8arr[n] = bstr.charCodeAt(n);
+    }
+    return u8arr;
   }
 }
